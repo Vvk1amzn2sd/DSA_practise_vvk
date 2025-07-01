1c9c6fbb999fc62d03adea5e4610becb9ade960c
Viv <115875842+Vvk1amzn2sd@users.noreply.github.com>
2025-07-01 16:36:45 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+
+// Firebase configuration for DSA Challenge project
+const firebaseConfig = {
+  apiKey: "AIzaSyBxmE8mAJJ5pFkOWC00ct_pWK1Autr2PAo",
+  authDomain: "dsa-challenge.firebaseapp.com",
+  databaseURL: "https://dsa-challenge-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dsa-challenge",
+  storageBucket: "dsa-challenge.firebasestorage.app",
+  messagingSenderId: "866070092409",
+  appId: "1:866070092409:web:49faada850b1b91bbf52a8",
+  measurementId: "G-P3Y7XMJN2L"
+};
+
+// Exporting so main.js can import it
+export default firebaseConfig;
